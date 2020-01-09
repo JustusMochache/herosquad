6b0e5e19d8387473600d21ee12001d84aa02525e
MoringaSchool <you@example.com>
2020-01-09 09:18:58 +0300

    identation of squad.java class

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -7,20 +7,33 @@ import java.util.List;
 public class Squad {
 
     private int squadId;
+
     private String squadName;
+
     private int squadSize;
+
     private String cause;
+
     private ArrayList<Hero>  squadMembers = new ArrayList<>();
+
     private static ArrayList<Squad> instances = new ArrayList<>();
 
 
     public Squad(String name, int size, String cause ){
+
         squadName = name;
+
         squadSize = size;
+
         this.cause = cause;
+
         this.squadMembers = new ArrayList<>();
+
         instances.add(this);
+
         this.squadId = instances.size();
+
+
 
     }
     public int getSquadId(){return squadId;}
